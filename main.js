f8ab8a67213f9a8829d0b6080925e354f144d6e0
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-27 16:23:17 +0100

    add style+ keep/delete function

--- main.js
+++ main.js
@@ -2,25 +2,50 @@ const emailList = []
 const olEl = document.querySelector(`.ol-element`)
 //console.log(olEl);
 
-for (let i = 0; i < 10; i++) {
-    fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
-        .then(response => response.json())
-        .then(data => {
-            renderLiElement(data.response)
-            emailList.push(data.response)
-        }
-        )
-        .catch(error => console.error(`Error:` + error))
+const buttonEl = document.getElementById(`new-api-call`)
+//console.log(buttonEl);
 
-}
+const checkboxEl = document.getElementById(`keep-generated`)
+console.log(checkboxEl);
+
+let keepEmails = false
+
+checkboxEl.addEventListener('change', function () {
+    keepEmails = !keepEmails
+    console.log(`keep old emails: ${keepEmails}`);
+})
+
+
+
+
+
+buttonEl.addEventListener('click', getNewEmails)
+
+
 
 
 console.log(emailList);
 
 //functions
+function getNewEmails(e) {
+    e.preventDefault()
+    if (keepEmails == false) {
+        olEl.innerHTML = ''
+    }
+    for (let i = 0; i < 10; i++) {
+        fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then(response => response.json())
+            .then(data => {
+                renderLiElement(data.response)
+                emailList.push(data.response)
+            })
+            .catch(error => console.error(`Error:` + error))
 
+    }
+    console.log(keepEmails);
+}
 function renderLiElement(response) {
-    const markup = `<li>${response}</li>`
+    const markup = `<li class="card">${response}</li>`
 
     olEl.innerHTML += markup
 }
