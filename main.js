1a3516077d64db888d08be101918086d28b63ddd
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-27 15:38:20 +0100

    add dom render

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const emailList = []
+const olEl = document.querySelector(`.ol-element`)
+//console.log(olEl);
 
 for (let i = 0; i < 10; i++) {
     fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
         .then(response => response.json())
         .then(data => {
+            renderLiElement(data.response)
             emailList.push(data.response)
         }
         )
@@ -13,4 +16,12 @@ for (let i = 0; i < 10; i++) {
 
 
 console.log(emailList);
+
+//functions
+
+function renderLiElement(response) {
+    const markup = `<li>${response}</li>`
+
+    olEl.innerHTML += markup
+}
 
